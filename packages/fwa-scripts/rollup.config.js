faecce912ce63085f5b67045d27c400f93ed71d0
grodier <grodier3@gmail.com>
2022-07-07 22:38:01 -0400

    Write build changes to playground

--- packages/fwa-scripts/rollup.config.js
+++ packages/fwa-scripts/rollup.config.js
@@ -2,6 +2,10 @@ import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import path from "path";
 import { preserveShebangs } from "rollup-plugin-preserve-shebangs";
+import fs from "fs";
+import fse from "fs-extra";
+
+const packageJson = JSON.parse(fs.readFileSync("./package.json"));
 
 function isBareModuleId(id) {
   return !id.startsWith(".") && !path.isAbsolute(id);
@@ -23,8 +27,38 @@ let fwaScripts = {
       extensions: [".ts"],
     }),
     preserveShebangs(),
+    copyToPlaygrounds(),
   ],
 };
+
+function copyToPlaygrounds() {
+  return {
+    name: "copy-to-fwa-playground",
+    async writeBundle(options) {
+      let playgroundsDir = path.join(__dirname, "../../playgrounds");
+
+      if (!fse.existsSync(playgroundsDir)) {
+        return;
+      }
+      let playgrounds = await fs.promises.readdir(playgroundsDir);
+      let writtenDir = path.join(__dirname, options.dir);
+      for (let playground of playgrounds) {
+        let playgroundDir = path.join(playgroundsDir, playground);
+        if (!fse.statSync(playgroundDir).isDirectory()) {
+          continue;
+        }
+        let destDir = path.join(
+          playgroundDir,
+          "node_modules",
+          packageJson.name,
+          "dist"
+        );
+        await fse.copy(writtenDir, destDir);
+        //await triggerLiveReload(playgroundDir);
+      }
+    },
+  };
+}
 
 export default function rollup() {
   return [fwaScripts];
