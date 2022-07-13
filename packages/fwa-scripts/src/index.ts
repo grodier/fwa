71bd1ec398e98d05e4fa1f809fa35c2cda1b2448
grodier <grodier3@gmail.com>
2022-07-13 00:27:16 -0400

    Await program in async cli

--- packages/fwa-scripts/src/index.ts
+++ packages/fwa-scripts/src/index.ts
@@ -3,7 +3,7 @@ import { build, dev, create } from "./commands/index.js";
 
 export async function cli(argv: string[]): Promise<void> {
   let program = new Command();
-  program
+  await program
     .version("0.1.0")
     .description("fwa scripts")
     .addCommand(build)
