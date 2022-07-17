57fa649505ed1fcc678e6b4c5e04d7365a72349c
grodier <grodier3@gmail.com>
2022-07-17 18:31:16 -0400

    Rfactor dev server in anticipation of watcher

--- packages/fwa-scripts/src/commands/dev/devServer.ts
+++ packages/fwa-scripts/src/commands/dev/devServer.ts
@@ -14,17 +14,47 @@ installGlobals();
 
 export async function devServer() {
   let app = express();
-
+  let router = express.Router();
   let config = await readConfig();
+
+  app.use(async (req, res, next) => {
+    router = express.Router();
+    let routeAssets: RouteAssetManifest = JSON.parse(
+      await fs.readFile(
+        path.join(config.buildPath, "route-asset-manifest.json"),
+        "utf-8"
+      )
+    );
+    for (let route of Object.values(routeAssets)) {
+      let routeModule = await import(route.modulePath);
+      router.all(route.serverPath, async (req, res, next) => {
+        let nodeResponse: NodeResponse = routeModule.default();
+        res.statusMessage = nodeResponse.statusText;
+        res.status(nodeResponse.status);
+
+        for (let [key, values] of Object.entries(nodeResponse.headers.raw())) {
+          for (let value of values) {
+            res.append(key, value);
+          }
+        }
+
+        if (nodeResponse.body) {
+          await writeReadableStreamToWritable(nodeResponse.body, res);
+        } else {
+          res.end();
+        }
+      });
+    }
+    next();
+  });
+
+  app.use((req, res, next) => {
+    router(req, res, next);
+  });
 
   let server: Server | null;
   let closeWatcher = await compiler.watch(config, {
     async onInitialBuild() {
-      app.use(
-        await createLocalRoutes(
-          path.join(config.buildPath, "route-asset-manifest.json")
-        )
-      );
       server = app.listen(3000, () => {
         console.log(`FWA started at http://localhost:3000`);
       });
@@ -41,33 +71,4 @@ export async function devServer() {
     await closeWatcher();
     server!?.close();
   });
-}
-
-async function createLocalRoutes(routeManifestPath: string) {
-  let app = express();
-  let routeAssets: RouteAssetManifest = JSON.parse(
-    await fs.readFile(routeManifestPath, "utf-8")
-  );
-
-  for (let route of Object.values(routeAssets)) {
-    let routeModule = await import(route.modulePath);
-    app.all(route.serverPath, async (req, res, next) => {
-      let nodeResponse: NodeResponse = routeModule.default();
-      res.statusMessage = nodeResponse.statusText;
-      res.status(nodeResponse.status);
-
-      for (let [key, values] of Object.entries(nodeResponse.headers.raw())) {
-        for (let value of values) {
-          res.append(key, value);
-        }
-      }
-
-      if (nodeResponse.body) {
-        await writeReadableStreamToWritable(nodeResponse.body, res);
-      } else {
-        res.end();
-      }
-    });
-  }
-  return app;
 }
